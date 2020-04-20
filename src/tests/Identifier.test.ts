f0f540a9ea9a53eb19f22babb4c09d62120eb726
underscoreanuj <underscoreanuj@gmail.com>
2020-04-20 21:39:55 +0530

    added CRDT + localIsert + tests

--- src/tests/Identifier.test.ts
+++ src/tests/Identifier.test.ts
@@ -2,6 +2,17 @@ import Identifier from "../lib/Identifier";
 
 describe("Identifier: compareTo", () => {
 
+    // tests for same identifier
+    it("returns 0 if the digit and site are same", () => {
+        const siteID1 = 1;
+        const siteID2 = 1;
+        const id1 = new Identifier(2, siteID1);
+        const id2 = new Identifier(2, siteID2);
+
+        expect(id1.compareTo(id2)).toEqual(0);
+    });
+
+    // tests for smaller identifier
     it("returns -1 if the digit is SMALLER", () => {
         const siteID1 = 1;
         const siteID2 = 20;
@@ -20,6 +31,7 @@ describe("Identifier: compareTo", () => {
         expect(id1.compareTo(id2)).toEqual(-1);
     });
 
+    // tests for larger identifier
     it("returns 1 if the digit is LARGER", () => {
         const siteID1 = 1;
         const siteID2 = 20;
@@ -36,15 +48,6 @@ describe("Identifier: compareTo", () => {
         const id2 = new Identifier(2, siteID2);
 
         expect(id1.compareTo(id2)).toEqual(1);
-    });
-
-    it("returns 0 if the digit and site are same", () => {
-        const siteID1 = 1;
-        const siteID2 = 1;
-        const id1 = new Identifier(2, siteID1);
-        const id2 = new Identifier(2, siteID2);
-
-        expect(id1.compareTo(id2)).toEqual(0);
     });
 });
 
